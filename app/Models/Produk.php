b48b4ac06c048b3d3dc80cb57fc7ac72ce01f6c2
aldmf26 <aldimf26@gmail.com>
2024-11-22 11:04:41 +0800

    upda

--- app/Models/Produk.php
+++ app/Models/Produk.php
@@ -30,7 +30,7 @@ class Produk extends Model
 
     public static function getAllProduk($search = null, $selectedTag = null)
     {
-        $query = Produk::with(['rak', 'pemilik', 'satuan'])->orderBy('nama_produk', 'ASC');
+        $query = Produk::with(['rak', 'pemilik', 'satuan'])->orderBy('foto', 'ASC');
 
         // Filter berdasarkan pencarian
         if ($search) {
