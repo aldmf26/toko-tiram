41db55df46ac8bee43e571f3d698090b7d1d88c6
aldmf26 <aldimf26@gmail.com>
2025-07-29 14:54:23 +0800

    dsa

--- app/Http/Livewire/Saldo.php
+++ app/Http/Livewire/Saldo.php
@@ -5,6 +5,7 @@ namespace App\Http\Livewire;
 use App\Models\Produk;
 use App\Models\Saldo as ModelsSaldo;
 use App\Models\TransaksiStok;
+use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 use Illuminate\Support\Facades\Http;
 
@@ -20,19 +21,19 @@ class Saldo extends Component
 
     public function update()
     {
-        $this->isLoading = true;    
+        $this->isLoading = true;
         ModelsSaldo::create([
             'saldo' => $this->saldo,
             'tgl' => date('Y-m-d'),
             'admin' => auth()->user()->name
         ]);
-        $this->isLoading = false;   
-        $this->dispatch('close-modal', 'tambah'); 
+        $this->isLoading = false;
+        $this->dispatch('close-modal', 'tambah');
     }
     public function render()
     {
         $ttlRpPenjualan = TransaksiStok::where('jenis_transaksi', 'penjualan')->sum('ttl_rp');
-        $ttlHarga = Produk::sum('harga');
+        $ttlHarga = Produk::sum(DB::raw('harga * stok'));
 
         $data = [
             'sisaSaldo' => $this->saldo - $ttlRpPenjualan + $ttlHarga
